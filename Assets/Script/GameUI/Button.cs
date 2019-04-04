42d1dd54078555eafe91f1895f0a0744f223d45f
uehara_yu <yuutopia.sg77@gmail.com>
2019-04-05 04:21:18 +0900

    SHOOTEXIT

---
+++ Assets/Script/GameUI/Button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    bool rightmove;
+    bool leftmove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightmove == true)
+        {
+            transform.position += new Vector3(1 * Time.deltaTime, 0, 0);
+        }
+        if (leftmove == true)
+        {
+            transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
+        }
+    }
+    public void rightButtonUP(){
+        rightmove = true;
+    }
+    public void rightButtonDOWN(){
+        rightmove = false;
+    }
+
+    public void leftButtonUP(){
+        leftmove = true;
+    }
+    public void leftButtonDOWN(){
+        leftmove = false;
+    }
+}
